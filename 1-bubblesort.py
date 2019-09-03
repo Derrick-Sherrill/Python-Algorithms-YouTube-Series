0d000db45905389327e2d86e4b7a5e00d8b914e2
Derrick Sherrill <36718839+Derrick-Sherrill@users.noreply.github.com>
2019-09-03 10:25:26 +0100

    Add bubble sort algorithm

---
+++ 1-bubblesort.py
@@ -0,0 +1,17 @@
+
+
+def bubble(unsorted_list):
+    indexing_length = len(unsorted_list) - 1 # indexing starts at zero in python, so we need one less " - 1"
+    sorted = False #Create variable of sorted and set it equal to false
+
+    while not sorted:  #Repeat until sorted = True
+        sorted = True  # Break the while loop whenever we have gone through all the values
+
+        for i in range(0, indexing_length): # For every value in the list
+            if unsorted_list[i] > unsorted_list[i+1]: #if value in list is greater than value directly to the right of it,
+                sorted = False # These values are unsorted
+                unsorted_list[i], unsorted_list[i+1] = unsorted_list[i+1], unsorted_list[i] #Switch these values
+    return unsorted_list # Return our list "unsorted_list" which is not sorted.
+
+
+print(bubble([4,8,1,14,8,2,9,5,7,6,6]))
