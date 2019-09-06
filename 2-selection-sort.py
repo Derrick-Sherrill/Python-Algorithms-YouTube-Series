326da83fc95a8b50cfa52636c7911acdf16ae5f1
Derrick Sherrill <36718839+Derrick-Sherrill@users.noreply.github.com>
2019-09-06 12:03:34 +0100

    Video #2 Selection Sort

---
+++ 2-selection-sort.py
@@ -0,0 +1,17 @@
+
+def selection_sort(list_a):
+    indexing_length = range(0, len(list)-1)
+
+    for i in indexing_length:
+        min_value = i
+
+        for j in range(i+1, len(list_a)):
+            if list_a[j] < list_a[min_value]:
+                min_value = j
+
+            if min_value != i:
+                list_a[min_value], list_a[i] = list_a[i], list_a[min_value]
+
+    return list_a
+
+print(selection_sort([6,7,8,7,6,5,4,5,6,7,6,7,8,9,7,9,0]))
