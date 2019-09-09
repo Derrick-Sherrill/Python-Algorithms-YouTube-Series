bf0a6cff06d0f026f04007a001f3ac557d2c1de5
Derrick Sherrill <36718839+Derrick-Sherrill@users.noreply.github.com>
2019-09-09 20:59:23 +0100

    Add Quick Sort script

---
+++ 4-Quick_Sort.py
@@ -0,0 +1,21 @@
+
+def quick_sort(sequence):
+    length = len(sequence)
+    if length <= 1:
+        return sequence
+    else:
+        pivot = sequence.pop()
+
+    items_greater = []
+    items_lower = []
+
+    for item in sequence:
+        if item > pivot:
+            items_greater.append(item)
+
+        else:
+            items_lower.append(item)
+
+    return quick_sort(items_lower) + [pivot] + quick_sort(items_greater)
+
+print(quick_sort([5,6,7,8,9,8,7,6,5,6,7,8,9,0]))
